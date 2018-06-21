f6076e37155d609fe9a570b323db2f445bd7ffec
Vivek Nimkarde <viveknimkarde59@gmail.com>
2018-06-21 16:10:59 +0530

    Add tests for sync and async response tests

--- test/test_denoise_api.py
+++ test/test_denoise_api.py
@@ -12,10 +12,11 @@ from __future__ import absolute_import
 import os
 import sys
 import unittest
-
+import uuid
 import deepaffects
 from deepaffects.models.audio import Audio
 from .test_base_setup import DIR
+
 
 
 class TestDenoiseApi(unittest.TestCase):
@@ -29,9 +30,21 @@ class TestDenoiseApi(unittest.TestCase):
     def setUp(self):
         deepaffects.configuration.api_key['apikey'] = os.environ['DEEPAFFECTS_API_KEY']
         self.api = deepaffects.DenoiseApi()
+        self.webhook_url = os.environ["DEEPAFFECTS_API_WEBHOOK"]
 
     def tearDown(self):
         pass
+    def test_async_denoise_audio(self):
+        """
+        Test case for sync_denoise_audio
+
+        Denoise an audio file 
+        """
+        self.request_id = str(uuid.uuid4())
+        test_noisy_audio = os.path.normpath(os.path.join(DIR, "data/noisy.wav"))
+        body = Audio.from_file(file_name=test_noisy_audio)
+        api_response = self.api.async_denoise_audio(body=body, webhook=self.webhook_url, request_id=self.request_id)
+        assert api_response.request_id, self.request_id
 
     def test_sync_denoise_audio(self):
         """
@@ -45,8 +58,7 @@ class TestDenoiseApi(unittest.TestCase):
         body = Audio.from_file(file_name=test_noisy_audio)
         api_response = self.api.sync_denoise_audio(body=body)
         api_response.to_file(test_reconstructed_audio)
-        self.assertTrue(TestDenoiseApi.sdr(test_clean_audio, test_reconstructed_audio) > 5)
-        pass
+        assert TestDenoiseApi.sdr(test_clean_audio, test_reconstructed_audio) > 5        
 
 
 if __name__ == '__main__':
--- test/test_emotion_api.py
+++ test/test_emotion_api.py
@@ -15,12 +15,13 @@ import sys
 import unittest
 
 from deepaffects import Audio
-
+import uuid
 import deepaffects
 from deepaffects.rest import ApiException
 from deepaffects.apis.emotion_api import EmotionApi
 from deepaffects.models.emotion_score import EmotionScore
-from .test_base_setup import DIR, AudioTest
+from .test_base_setup import DIR
+
 
 
 class TestEmotionApi(unittest.TestCase):
@@ -28,18 +29,35 @@ class TestEmotionApi(unittest.TestCase):
 
     def setUp(self):
         deepaffects.configuration.api_key['apikey'] = os.environ['DEEPAFFECTS_API_KEY']
+        self.webhook_url = os.environ["DEEPAFFECTS_API_WEBHOOK"]                
         self.api = deepaffects.apis.emotion_api.EmotionApi()
 
     def tearDown(self):
         pass
 
-    def test_async_recognise_emotion(self):
+    def test_async_recognise_emotion_with_request_id(self):
         """
         Test case for async_recognise_emotion
 
         Find emotion in an audio file
         """
-        pass
+        self.request_id = str(uuid.uuid4())
+        test_happy_audio = os.path.normpath(os.path.join(DIR, "data/happy.mp3"))
+        body = Audio.from_file(file_name=test_happy_audio)        
+        api_response = self.api.async_recognise_emotion(body=body, webhook=self.webhook_url, request_id=self.request_id)
+        print(api_response)
+        assert api_response.request_id, self.request_id
+    def test_async_recognise_emotion_without_request_id(self):
+        """
+        Test case for async_recognise_emotion
+
+        Find emotion in an audio file
+        """
+        test_happy_audio = os.path.normpath(os.path.join(DIR, "data/happy.mp3"))
+        body = Audio.from_file(file_name=test_happy_audio)        
+        api_response = self.api.async_recognise_emotion(body=body, webhook=self.webhook_url)
+        print(api_response)
+        assert api_response.request_id
 
     def test_sync_recognise_emotion(self):
         """
@@ -50,6 +68,7 @@ class TestEmotionApi(unittest.TestCase):
         test_happy_audio = os.path.normpath(os.path.join(DIR, "data/happy.mp3"))
         body = Audio.from_file(file_name=test_happy_audio)
         api_response = self.api.sync_recognise_emotion(body=body)
+        print(api_response)
         for obj in api_response:
             if obj.emotion == 'Happy':
                 assert obj.score > 0.8
