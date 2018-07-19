b09702b91afee47197c8a3667262e5913201378a
Vivek Nimkarde <viveknimkarde59@gmail.com>
2018-07-19 14:19:31 +0530

    Add diarize emotion endpoint

--- examples/diarize_emotion_example.py
+++ examples/diarize_emotion_example.py
@@ -1,62 +1,62 @@
 from deepaffects.realtime.util import get_deepaffects_client, chunk_generator_from_file, chunk_generator_from_url
 
-TIMEOUT_SECONDS = 3000
-apikey = "Pnpr9E0nEWEdVByJoGxquE7EPjbFLB0W"
-file_path = "/Users/VivekNimkarde/DeepAffects/my_files/TSLA-q1-2018.mp3"
+TIMEOUT_SECONDS = 2000
+apikey = "YOUR_API_KEY"
+file_path = "FILE_PATH"
+is_youtube_url = False
 languageCode = "en-Us"
 sampleRate = "16000"
-encoding = "mp3"
-userIds = "ElonMusk"
+encoding = "wav"
+userIds = "list of userids for for speaker verification seperated by ','"
 # DeepAffects realtime Api client
 client = get_deepaffects_client()
 
-# chunk_generator() is a generator function which yields audio segment object asynchronously
 metadata = [
     ('apikey', apikey),
-    ('userids', userIds),
     ('encoding', encoding),
+    ('userids', userIds),
     ('samplerate', sampleRate),
     ('languagecode', languageCode)
 ]
 
+# Implement chunk_generator() is a generator function which yields segment_chunk objects asynchronously
+# from deepaffects.realtime.types import segment_chunk
+# yield segment_chunk(Args)
+"""segment_chunk.
+
+Args:
+    encoding : Audio Encoding,
+    languageCode: language code ,
+    sampleRate: sample rate of audio ,
+    content: base64 encoded audio,
+    segmentOffset: offset of the segment in complete audio stream
+"""
+
+"""
+Sample implementation which reads audio from a file and splits it into
+segments more than 3 sec
+AudioSegment and yields base64 encoded audio segment objects asynchronously
+"""
+
 """Stream audio from url or youtube.
 
-responses = client.IdentifySpeaker(
+responses = client.IdentifyEmotion(
     chunk_generator_from_url(file_path, is_youtube_url=is_youtube_url), TIMEOUT_SECONDS, metadata=metadata)
 """
 
 """Stream audio from local file.
 """
-generator = chunk_generator_from_file(file_path)
-
-responsesSpeaker = client.IdentifySpeaker(
-    generator, TIMEOUT_SECONDS, metadata=metadata)
-responsesEmotion = client.IdentifyEmotion(
-    generator, TIMEOUT_SECONDS, metadata=metadata)
+responses = client.DiarizeEmotion(
+    chunk_generator_from_file(file_path), TIMEOUT_SECONDS, metadata=metadata)
 
 # responses is the iterator for all the response values
-for response in responsesSpeaker:
+for response in responses:
     print("Received message")
     print(response)
-
-
-# responses is the iterator for all the response values
-for response in responsesEmotion:
-    print("Received message")
-    print(response)
-
-# responses = client.IdentifyEmotion(
-#     chunk_generator_from_file(file_path), TIMEOUT_SECONDS, metadata=metadata)
-
-# # responses is the iterator for all the response values
-# for response in responses:
-#     print("Received message")
-#     print(response)
 
 """Response.
-
-    response = {           
-        speaker: speaker identified in the segment,
+    response = {
+        emotion: Emotion identified in the segment,
         start: start of the segment,
         end: end of the segment
     }
