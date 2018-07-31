6da959e9ae1b3dfbeb7f1c47ce08b2ee5e18ea98
Sushant Hiray <hiraysushant@gmail.com>
2018-07-31 07:31:30 +0530

    Release v1.1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 import os
 
 NAME = "deepaffects"
-VERSION = "1.1.7"
+VERSION = "1.1.8"
 # To install the library, run the following
 #
 # python setup.py install
