b72a9d9c08312972475db61b857308c7fbbc0a75
Praneeth <praneethbedapudi@gmail.com>
2019-06-27 21:59:02 +0530

    initial commit for refactor

--- deepaffects/apis/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-# import apis into api package
-from .denoise_api import DenoiseApi
-from .diarize_api import DiarizeApi
-from .emotion_api import EmotionApi
-from .featurize_api import FeaturizeApi
-from .ellipsis_api import EllipsisAPI
-from .diarize_api_v2 import DiarizeApiV2
